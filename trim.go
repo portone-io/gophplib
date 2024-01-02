88ec8a253035923683faa2de6d3bfb372c790873
mae <mae@chai.finance>
2024-01-02 19:51:32 +0900

    trim: Add trim function for go and test cases

---
+++ trim.go
@@ -0,0 +1,93 @@
+package gophplib
+
+import (
+	"fmt"
+	"strings"
+)
+
+type toStringAble interface {
+	toString() string
+}
+
+// Trim is a ported function that works exactly the same as PHP 5.6's trim
+// function. For more information, see the [official PHP documentation].
+//
+// In PHP 5.6, when attempting to use the trim() function with a data type other
+// than a string, it automatically converts the requested variable into a string
+// before performing the trim. To achieve the same behavior in Go, this function
+// converts the requested data types into strings and then utilize the trim
+// function from the package strings. For more detailed information about the
+// trim function in the package strings, see the [strings's trim documentation]
+//
+// NOTE: Implementation of second parameter of Trim is not complete yet. This
+// function does not support specifying a set of characters using string ranges
+// (e.g., "a..z") in the chars parameter.
+//
+// References:
+//   - https://www.php.net/manual/en/function.trim.php
+//   - https://github.com/php/php-src/blob/php-5.6.40/ext/standard/string.c#L840-L850
+//   - https://github.com/php/php-src/blob/php-5.6.40/Zend/zend_API.c#L425-L470
+//   - https://github.com/php/php-src/blob/php-5.6.40/Zend/zend_operators.c#L593-L661
+//   - https://github.com/php/php-src/blob/php-5.6.40/Zend/zend_API.c#L261-L302
+//
+// Test Cases:
+//   - https://github.com/php/php-src/blob/php-5.6.40/ext/standard/tests/strings/trim1.phpt
+//   - https://github.com/php/php-src/blob/php-5.6.40/ext/standard/tests/strings/trim.phpt
+//   - https://github.com/php/php-src/blob/php-5.6.40/ext/standard/tests/strings/trim_basic.phpt
+//   - https://github.com/php/php-src/blob/php-5.6.40/ext/standard/tests/strings/trim_variation1.phpt
+//
+// [official PHP documentation]: https://www.php.net/manual/en/function.trim.php
+// [strings's trim documentation]: https://pkg.go.dev/strings#Trim
+func Trim(value any, chars ...any) any {
+	var charSet string
+
+	// If the `chars` is not provided or is an empty string, set the default character set
+	if len(chars) == 0 {
+		charSet = " \n\r\t\v\x00"
+	} else {
+		// If `chars` is a string, use it as the character set.
+		// Otherwise, set an empty string as the character set.
+		switch v := chars[0].(type) {
+		case string:
+			charSet = v
+		default:
+			charSet = ""
+		}
+	}
+
+	// Handle different types of value
+	switch v := value.(type) {
+	case string:
+		return strings.Trim(v, charSet)
+	case int:
+		return strings.Trim(fmt.Sprintf("%v", value), charSet)
+	case float64:
+		// Rules for converting a float to a string in PHP 5.6:
+		// - Allows up to a maximum of 14 digits, including both integer and decimal places.
+		// - Remove trailing zeros from the fractional part
+		//	 ex) 123.4000 → "123.4"
+		// - Keep the values as is if the last digit is not 0.
+		//   ex) 123.45 → "123.45"
+		// - If the integer part exceeds 14 digits, use exponential notation.
+		//   ex) 123456789123456.40 → "1.2345678901234e+14"
+		// - If the total number of digits exceeds 14, truncate the decimal places.
+		//   ex) 123.45678901234 → "123.4567890123"
+		result := fmt.Sprintf("%.*G", 14, v)
+		return strings.Trim(result, charSet)
+	case bool:
+		// return "1" for true and an empty string("") for false
+		if v {
+			return "1"
+		} else {
+			return ""
+		}
+	case nil:
+		return ""
+	case toStringAble:
+		// For types implementing toString(), trim the return value of toString()
+		return strings.Trim(v.toString(), charSet)
+	default:
+		// For other types, return nil
+		return nil
+	}
+}
---
+++ trim_test.go
@@ -0,0 +1,369 @@
+package gophplib
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type Cat struct {
+	name string
+	age  int
+}
+
+type Dog struct {
+	name string
+	age  int
+}
+
+func (c Cat) toString() string {
+	return fmt.Sprintf("name is %s and %d years old", c.name, c.age)
+}
+
+func customTrim(value interface{}) string {
+	return " hello world "
+}
+
+func ExampleTrim_string() {
+	// Trim plain string
+	fmt.Println(Trim("Hello world "))
+
+	// Trim empty string
+	fmt.Println(Trim(""))
+
+	// Trim string with default characters
+	fmt.Println(Trim(" \x00\t\nABC \x00\t\n"))
+
+	// Output:
+	// Hello world
+	//
+	// ABC
+}
+
+func ExampleTrim_float() {
+	// Trim float
+	fmt.Println(Trim(123.40))
+
+	// Trim negative float
+	fmt.Println(Trim(-123.40))
+
+	// Trim exponent
+	fmt.Println(Trim(10.1234567e10))
+
+	// Trim float exceeds 14 digits
+	fmt.Println(Trim(1230.12984732591475609346509132875091237))
+
+	// Trim float ends with 0
+	fmt.Println(Trim(1230.12984732500000000000000000000000000))
+
+	// Trim integer exceeds 14 digits
+	fmt.Println(Trim(123456789123456.40))
+
+	// Trim integer ends with 0
+	fmt.Println(Trim(12345678912340.40))
+
+	// Output:
+	// 123.4
+	// -123.4
+	// 101234567000
+	// 1230.1298473259
+	// 1230.129847325
+	// 1.2345678912346E+14
+	// 12345678912340
+}
+
+func ExampleTrim_otherTypes() {
+	// Trim integer
+	fmt.Println(Trim(123))
+
+	// Trim negative integer
+	fmt.Println(Trim("-123"))
+
+	// Trim zero
+	fmt.Println(Trim(0))
+
+	// Trim empty array
+	fmt.Println(Trim([]interface{}{}))
+
+	// Trim bool (true)
+	fmt.Println(Trim(true))
+
+	// Trim bool (false)
+	fmt.Println(Trim(false))
+
+	// Trim object has toString
+	fmt.Println(Trim(Cat{name: "nabi", age: 3}))
+
+	// Trim object has no toString
+	fmt.Println(Trim(Dog{name: "choco", age: 5}))
+
+	// Trim function
+	fmt.Println(Trim(customTrim(nil)))
+
+	// Trim here documents
+	fmt.Println(Trim(`<header>
+	<h1>hello world   </h1>
+</header>`))
+
+	// Trim resource
+	file, err := os.Open("README.md")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(Trim(file))
+
+	// Output:
+	// 123
+	// -123
+	// 0
+	// <nil>
+	// 1
+	//
+	// name is nabi and 3 years old
+	// <nil>
+	// hello world
+	// <header>
+	//	<h1>hello world   </h1>
+	//</header>
+	// <nil>
+}
+
+func ExampleTrim_setCutset() {
+	// Trim with cutset is null
+	fmt.Println(strings.ReplaceAll(Trim("  \ttesting trim  ", nil).(string), " ", "[SPACE]"))
+
+	// Trim with cutset is true
+	fmt.Println(strings.ReplaceAll(Trim("  \ttesting trim ", true).(string), " ", "[SPACE]"))
+
+	// Trim with cutset is empty string
+	fmt.Println(strings.ReplaceAll(Trim("\ttesting trim", "").(string), " ", "[SPACE]"))
+
+	// Trim string with no white space
+	fmt.Println(Trim("!===Hello World===!", "=!"))
+
+	// Trim string with embedded null
+	fmt.Println(strings.ReplaceAll(Trim("\\x0n1234\x0005678\x0000efgh\\xijkl\\x0n1", "\\x0n1").(string), "\x00", "[NULL]"))
+
+	// Trim string with default characters
+	fmt.Println(strings.ReplaceAll(Trim(" \x00\t\nABC \x00\t\n", "").(string), "\x00\t\n", "[NULL][TAB][NEWLINE]"))
+
+	// Output:
+	// [SPACE][SPACE]	testing[SPACE]trim[SPACE][SPACE]
+	// [SPACE][SPACE]	testing[SPACE]trim[SPACE]
+	// 	testing[SPACE]trim
+	// Hello World
+	// 234[NULL]05678[NULL]00efgh\xijkl
+	//  [NULL][TAB][NEWLINE]ABC [NULL][TAB][NEWLINE]
+}
+
+func TestTrim(t *testing.T) {
+	c := Cat{
+		name: "nabi",
+		age:  3,
+	}
+
+	d := Dog{
+		name: "Choco",
+		age:  4,
+	}
+	file, err := os.Open("README.md")
+	if err != nil {
+		panic(err)
+	}
+
+	testCase := []struct {
+		testName string
+		input    interface{}
+		expected interface{}
+	}{
+		{
+			testName: "BasicTest",
+			input:    "Hello world ",
+			expected: "Hello world",
+		},
+		{
+			testName: "EmptyString",
+			input:    "",
+			expected: "",
+		},
+		{
+			testName: "Integer",
+			input:    123,
+			expected: "123",
+		},
+		{
+			testName: "NegativeInteger",
+			input:    -123,
+			expected: "-123",
+		},
+		{
+			testName: "Zero",
+			input:    0,
+			expected: "0",
+		},
+		{
+			testName: "EmptyArray",
+			input:    []interface{}{},
+			expected: nil,
+		},
+		{
+			testName: "Array",
+			input:    []interface{}{"foo", "456", 7},
+			expected: nil,
+		},
+		{
+			testName: "True",
+			input:    true,
+			expected: "1",
+		},
+		{
+			testName: "False",
+			input:    false,
+			expected: "",
+		},
+		{
+			testName: "Nil",
+			input:    nil,
+			expected: "",
+		},
+		{
+			testName: "Float",
+			input:    123.40,
+			expected: "123.4",
+		},
+		{
+			testName: "NegativeFloat",
+			input:    -123.40,
+			expected: "-123.4",
+		},
+		{
+			testName: "Exponent",
+			input:    10.1234567e10,
+			expected: "101234567000",
+		},
+		{
+			testName: "FloatExceeds14Digits",
+			input:    1230.12984732591475609346509132875091237,
+			expected: "1230.1298473259",
+		},
+		{
+			testName: "FloatEndsWith0",
+			input:    1230.12984732500000000000000000000000000,
+			expected: "1230.129847325",
+		},
+		{
+			testName: "IntegerExceeds14Digits",
+			input:    123456789123456.40,
+			expected: "1.2345678912346E+14",
+		},
+		{
+			testName: "IntegerEndsWith0",
+			input:    12345678912340.40,
+			expected: "12345678912340",
+		},
+		{
+			testName: "ObjectHasToString",
+			input:    c,
+			expected: c.toString(),
+		},
+		{
+			testName: "ObjectWithoutToString",
+			input:    d,
+			expected: nil,
+		},
+		{
+			testName: "Function",
+			input:    customTrim(nil),
+			expected: "hello world",
+		},
+		{
+			testName: "HereDocuments",
+			input: `<header>
+	<h1>hello world   </h1>
+</header>`,
+			expected: "<header>\n\t<h1>hello world   </h1>\n</header>",
+		},
+		{
+			testName: "Resource",
+			input:    file,
+			expected: nil,
+		},
+		{
+			testName: "StringWithDefaultCharacters",
+			input:    " \x00\t\nABC \x00\t\n",
+			expected: "ABC",
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Trim(tc.input)
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("%s: expected %v, bug got %v", tc.testName, tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestTrimWithCutset(t *testing.T) {
+	testCase := []struct {
+		testName string
+		input    interface{}
+		cutset   interface{}
+		expected interface{}
+	}{
+		{
+			testName: "cutsetIsNull",
+			input:    "  \ttesting trim ",
+			cutset:   nil,
+			expected: "  \ttesting trim ",
+		},
+		{
+			testName: "cutsetIsTrue",
+			input:    "  \ttesting trim ",
+			cutset:   true,
+			expected: "  \ttesting trim ",
+		},
+		{
+			testName: "cutsetIsEmptyString",
+			input:    "\ttesting trim",
+			cutset:   "",
+			expected: "\ttesting trim",
+		},
+		{
+			testName: "StringWithEmbeddedNull",
+			input:    "\\x0n1234\x0005678\x0000efgh\\xijkl\\x0n1",
+			cutset:   "\\x0n1",
+			expected: "234\x0005678\x0000efgh\\xijkl",
+		},
+		{
+			testName: "StringWithDefaultCharacters",
+			input:    " \x00\t\nABC \x00\t\n",
+			cutset:   "",
+			expected: " \x00\t\nABC \x00\t\n",
+		},
+		{
+			testName: "StringWithNoWhitespace",
+			input:    "!===Hello World===!",
+			cutset:   "=!",
+			expected: "Hello World",
+		},
+		{
+			testName: "cutsetIsRangeWithASCIICode",
+			input:    "abcdefghi",
+			cutset:   "a..f",
+			expected: "bcdefghi",
+		},
+	}
+	for _, tc := range testCase {
+		t.Run(tc.testName, func(t *testing.T) {
+			result := Trim(tc.input, tc.cutset)
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("%s: expected %v, bug got %v", tc.testName, tc.expected, result)
+			}
+		})
+	}
+
+}
