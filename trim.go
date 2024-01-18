ccea343cf2cd24a7c94ccdc6e4222e801808f64d
Hyeon Kim <simnalamburt@gmail.com>
2024-01-18 19:00:22 +0900

    trim.go: Change interface of Trim() function

--- trim.go
+++ trim.go
@@ -2,6 +2,7 @@ package gophplib
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -33,42 +34,16 @@ import (
 //
 // [official PHP documentation]: https://www.php.net/manual/en/function.trim.php
 // [strings's trim documentation]: https://pkg.go.dev/strings#Trim
-func Trim(value any) any {
-	var charSet = " \n\r\t\v\x00"
-
-	// Handle different types of value
-	switch v := value.(type) {
-	case string:
-		return strings.Trim(v, charSet)
-	case int:
-		return strings.Trim(fmt.Sprintf("%v", value), charSet)
-	case float64:
-		// Rules for converting a float to a string in PHP 5.6:
-		// - Allows up to a maximum of 14 digits, including both integer and decimal places.
-		// - Remove trailing zeros from the fractional part
-		//	 ex) 123.4000 → "123.4"
-		// - Keep the values as is if the last digit is not 0.
-		//   ex) 123.45 → "123.45"
-		// - If the integer part exceeds 14 digits, use exponential notation.
-		//   ex) 123456789123456.40 → "1.2345678901234e+14"
-		// - If the total number of digits exceeds 14, truncate the decimal places.
-		//   ex) 123.45678901234 → "123.4567890123"
-		result := fmt.Sprintf("%.*G", 14, v)
-		return strings.Trim(result, charSet)
-	case bool:
-		// return "1" for true and an empty string("") for false
-		if v {
-			return "1"
-		} else {
-			return ""
-		}
-	case nil:
-		return ""
-	case toStringAble:
-		// For types implementing toString(), trim the return value of toString()
-		return strings.Trim(v.toString(), charSet)
-	default:
-		// For other types, return nil
-		return nil
+func Trim(value any) (ret string, err error) {
+	// Convert a value to string
+	characterString, err := zendParseArgAsString(value)
+	if err != nil {
+		err = fmt.Errorf("unsupported type : %s", reflect.TypeOf(value))
+		return
 	}
+
+	const charSet = " \n\r\t\v\x00"
+
+	ret = strings.Trim(characterString, charSet)
+	return
 }
