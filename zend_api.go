4571ea0d572172a600e11fc12bdd103e16d78f28
mae <mae@chai.finance>
2024-02-16 11:53:37 +0900

    zend_api.go: Renamed filename and refactor to use ConvertToString
    - renamed filename to match the PHP file format
    - refactor `zendParseArgAsString`, replacing direct string conversion
    logic for string, int, float, and bool types with calls to
    ConvertToString
    - extend support to int and float types

--- zend_api.go
+++ zend_api.go
@@ -7,7 +7,8 @@ import (
 
 // zendParseArgAsString attempts to replicate the behavior of the 'zend_parse_arg_impl' function
 // from PHP 5.6, specifically for the case where the 'spec' parameter is "s".
-// It handles conversion of different types to string in a way that aligns with PHP's type juggling rules.
+// It handles conversion of different types to string in a way that aligns with PHP's type juggling rules,
+// calling ConvertToString to manage string, int, float, and bool types, akin to PHP's _convert_to_string.
 //
 // This function returns error if given argument is not one of following:
 // string, int, int8, int16, int32, int64, float32, float64, bool, nil
@@ -22,19 +23,8 @@ func zendParseArgAsString(value any) (string, error) {
 	var str string
 
 	switch v := value.(type) {
-	case string:
-		str = v
-	case int, int64:
-		str = fmt.Sprintf("%v", v)
-	case float64:
-		str = floatToString(v)
-	case bool:
-		// return "1" for true and an empty string("") for false
-		if v {
-			str = "1"
-		} else {
-			str = ""
-		}
+	case string, int, int8, int16, int32, int64, float32, float64, bool:
+		return ConvertToString(value)
 	case nil:
 		// TODO: handle check_null
 		str = ""
